f40a19f9f7f21d2284b2d2f63fbd5662ffca327e
Ben Nuttall <ben@bennuttall.com>
2013-12-11 16:28:57 +0000

    Make it work for Apache 2.4

--- new_vhost.sh
+++ new_vhost.sh
@@ -1,17 +1,17 @@
 VHOSTNAME=$1
-SOURCE_PATH="/home/ben/Projects/$VHOSTNAME"
+SOURCE_PATH="/home/ben/Projects/web/$VHOSTNAME"
 
 # Copy vhost template
-cp vhost $VHOSTNAME
+cp vhost.conf "$VHOSTNAME.conf"
 
 # Replace "{VHOSTNAME}" with the vhost name
-sed -i "s|{VHOSTNAME}|$VHOSTNAME|g" $VHOSTNAME
+sed -i "s|{VHOSTNAME}|$VHOSTNAME|g" "$VHOSTNAME.conf"
 
 # Replace "PATH" with the path to the directory
-sed -i "s|{PATH}|$SOURCE_PATH|g" $VHOSTNAME
+sed -i "s|{SOURCE_PATH}|$SOURCE_PATH|g" "$VHOSTNAME.conf"
 
 # Move the new vhost file to sites-available
-mv $VHOSTNAME /etc/apache2/sites-available/$VHOSTNAME
+mv "$VHOSTNAME.conf" /etc/apache2/sites-available/
 
 # Enable the site
 a2ensite $VHOSTNAME
